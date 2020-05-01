98828daeb8e613094c22306587b4311c8dc5811a
Juan Miguel Alvarez <jm@Juans-MacBook-Pro.local>
2020-05-01 19:47:31 +0800

    fix header and navigation

--- app/models/guide.js
+++ app/models/guide.js
@@ -3,6 +3,6 @@ import Model, {attr} from '@ember-data/model';
 export default class GuideModel extends Model {
   @attr('string') title;
   @attr('string') image;
-  @attr('string') description; 
+  @attr('string') description;
   @attr('string') content;
 }
--- app/router.js
+++ app/router.js
@@ -12,4 +12,5 @@ Router.map(function() {
   this.route('guide', {
     path: 'guides/:guide_id'
   });
+  this.route('contribute');
 });
